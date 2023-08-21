2a18cc49ec589c72d69a111a6c8e8497719830c1
Juan <juan_jg@hotmail.com>
2023-08-21 10:11:16 -0300

    added setup para conexao typeorm-sqlite

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import 'reflect-metadata'
 import express from 'express'
+import { AppDataSource } from './database/data-source'
 
 const app = express()
 
@@ -6,6 +8,10 @@ app.use(express.json())
 app.get('/', (req, res) => {
   const { name } = req.body
   return res.json({ message: 'ok' })
+})
+
+AppDataSource.initialize().then((async) => {
+  console.log('Database connection ok')
 })
 
 export { app }
---
+++ src/database/data-source.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+
+export const AppDataSource = new DataSource({
+  type: 'sqlite',
+  host: 'localhost',
+  port: 4001,
+  username: 'root',
+  password: 'root',
+  database: './src/database/database.sqlite',
+  synchronize: false,
+  logging: false,
+  entities: ['./src/modules/users/entities/**.ts'],
+  migrations: ['./src/database/migrations/**.ts'],
+})
