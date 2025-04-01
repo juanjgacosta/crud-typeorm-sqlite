66f509a4c0ae6a311d98ce64da5ddd4fe4093de1
Juan <juan_jg@hotmail.com>
2025-04-01 16:17:03 -0300

    add NODE_ENV setup om data-source.ts file

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,20 +1,28 @@
 import { DataSource } from 'typeorm'
 
+const databaseFile =
+  process.env.NODE_ENV === 'test'
+    ? './src/database/database.test.sqlite'
+    : './src/database/database.sqlite'
+
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   // host: 'localhost',
   // port: 4001,
   // username: 'root',
   // password: 'root',
-  database: './src/database/database.sqlite',
+  // database: './src/database/database.sqlite',
+  database: databaseFile,
   synchronize: false,
   logging: false,
   entities: ['./src/modules/users/entities/**.ts'],
   migrations: ['./src/database/migrations/**.ts'],
 })
 
-AppDataSource.initialize().then((async) => {
-  console.log('Database connection ok')
-}).catch((err) => {
-  console.error('Database error connection: ', err)
-})
+AppDataSource.initialize()
+  .then((async) => {
+    console.log(`Database connection ok with: ${databaseFile}`)
+  })
+  .catch((err) => {
+    console.error('Database error connection: ', err)
+  })
