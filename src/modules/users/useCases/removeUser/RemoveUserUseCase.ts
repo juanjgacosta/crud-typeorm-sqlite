d2e9364f889d3a3d9a197e233c519bd95c419cc6
Juan <juan_jg@hotmail.com>
2025-10-24 11:19:08 -0300

    feat: add file deletion logic for user avatar during user removal and update

--- src/modules/users/useCases/removeUser/RemoveUserUseCase.ts
+++ src/modules/users/useCases/removeUser/RemoveUserUseCase.ts
@@ -2,11 +2,13 @@ import { inject, injectable } from 'tsyringe'
 import { AppError } from '../../../../errors/AppError'
 import { User } from '../../entities/User'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
+import { deleteFile } from '../../../../utils/file'
 @injectable()
 class RemoveUserUseCase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository) {}
+    private usersRepository: IUsersRepository
+  ) {}
 
   async execute(id: string): Promise<User> {
     const user = await this.usersRepository.findUserById(id)
@@ -15,7 +17,11 @@ class RemoveUserUseCase {
       throw new AppError('User not registered', 404)
     }
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    }
     await this.usersRepository.removeUser(id)
+
     return user
   }
 }
--- src/modules/users/useCases/updateUserAvatarUseCase/UpdateUserAvatarUseCase.ts
+++ src/modules/users/useCases/updateUserAvatarUseCase/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 import { AppError } from '../../../../errors/AppError'
+import { deleteFile } from '../../../../utils/file'
 
 interface IRequest {
   user_id: string
@@ -19,6 +20,10 @@ class UpdateUserAvatarUseCase {
       throw new AppError('User not registered', 404)
     }
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    }
+    
     user.avatar = avatar_file
 
     await this.usersRepository.createUser(user)
